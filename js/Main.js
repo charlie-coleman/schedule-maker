c1e34931fa514f61185ca1cd82439ae3b424f3d7
Charlie Coleman <coleman.charlie97@gmail.com>
2017-03-29 01:01:32 +0200

    Add: hour lines and labels

---
+++ js/Main.js
@@ -0,0 +1,56 @@
+var twentyfourhour = false;
+var generate_hourmarks = function(begin, end) {
+    var num_marks = end - begin;
+    var num_days = $('.day').length;
+    for(var i = 0; i < num_days; i++) {
+        $($('.day')[i]).find('.hour').remove();
+        for(var j = 0; j < num_marks; j++) {
+            $($('.day')[i]).append('<div class="hour"></div>');
+        }
+    }
+    var hour_width = Math.floor(($($('.day')[0]).height()-50)/num_marks);
+    $('.hour').css({
+        'height':hour_width.toString(),
+        'width':'100%',
+        'border-bottom': '1px solid #888'
+    });
+    for(var i = 0; i < num_marks+1; i++) {
+        var label = (begin + i);
+        if (!twentyfourhour){
+            labelnew = label % 12;
+            (labelnew == 0) ? labelnew = begin+i : labelnew = labelnew;
+            (labelnew == 24 || labelnew == 0) ? labelnew = 12 : labelnew = labelnew;
+            if(label < 12 || label == 24) {
+                label = labelnew.toString() + 'am';
+            }
+            else {
+                label = labelnew.toString() + 'pm';
+            }
+        }
+        label = label.toString();
+        $('.hour-marks').append('<div class="hour-mark" id="hm'+i+'">'+label+'</div>');
+        if(i != num_marks) {
+            var loc = $($('.hour')[i]).position().top - $(window).height()*0.012;
+            $('#hm'+i).css({
+                'position': 'absolute',
+                'top' : loc.toString(),
+                'left' : $('.hour-marks').position().left.toString()
+            });
+        }
+        else {
+            var loc = $($('.hour')[i-1]).position().top + $($('.hour')[i]).height()-$(window).height()*0.011;
+            $('#hm'+i).css({
+                'position': 'absolute',
+                'top' : loc.toString(),
+                'left' : $('.hour-marks').position().left.toString()
+            });
+        }
+    }
+    $('.hour-mark').css({
+        'width' : '3.3%',
+        'text-align' : 'right'
+    })
+}
+$(document).ready(function() {
+    generate_hourmarks(7,18);
+});
