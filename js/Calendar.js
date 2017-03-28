0d28a49b3030970856c4255c8a5b9ca7dd1195f5
Charlie Coleman <coleman.charlie97@gmail.com>
2017-03-28 20:40:53 +0200

    Add: Calendar and Class classes, HTML basic structure

---
+++ js/Calendar.js
@@ -0,0 +1,5 @@
+function Calendar(class_list) {
+    this.create_display = function(container) {
+        
+    }
+}
---
+++ js/Class.js
@@ -0,0 +1,6 @@
+function Class(name, crn, time, days) {
+    this.name = name;
+    this.crn  = crn;
+    this.time = time;
+    this.days = days;
+}
